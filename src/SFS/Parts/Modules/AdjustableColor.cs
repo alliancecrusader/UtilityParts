41e84cb909a85bae8daf21d67d2c52c52b8ef08e
Amir <139617633+alliancecrusader@users.noreply.github.com>
2025-07-27 15:46:45 +0930

    Changed AdjustableColor input to hex

--- src/SFS/Parts/Modules/AdjustableColor.cs
+++ src/SFS/Parts/Modules/AdjustableColor.cs
@@ -1,17 +1,22 @@
 ﻿using SFS.Variables;
+using System.Runtime.InteropServices;
 using UnityEngine;
 
 namespace SFS.Parts.Modules
 {
     public class AdjustableColor : ColorModule
     {
+        public Composed_String hex;
         public Composed_Float alpha;
-        public Composed_Float red;
-        public Composed_Float green;
-        public Composed_Float blue;
         public override Color GetColor()
         {
-            return new Color(red.Value, green.Value, blue.Value, alpha.Value);
+            if (ColorUtility.TryParseHtmlString(this.hex.Value, out Color newColor)) {
+                newColor.a = alpha.Value;
+                return newColor;
+            } else
+            {
+                return Color.white;
+            }
         }
     }
 }
